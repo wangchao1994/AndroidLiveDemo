5ce96182461e9ac1e2d5b19decfa60a0622a8439
wangchao <wchao0829@163.com>
2019-09-17 20:22:02 +0800

    来疯sdk

--- app/src/main/java/com/android/live/MainActivity.java
+++ app/src/main/java/com/android/live/MainActivity.java
@@ -10,7 +10,6 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
